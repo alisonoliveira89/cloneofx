716bb3eef5f032ef1718c38306b617746e1d05eb
Alison Oliveira <alison.oliveira89@gmail.com>
2025-05-09 16:52:56 -0300

    configure followers and automatic deploy

--- users/views.py
+++ users/views.py
@@ -1,17 +1,21 @@
+# users/views.py
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
-from .models import CustomUser
+from .models import CustomUser, Follow
 from .serializers import UserSerializer
+
+from rest_framework.permissions import AllowAny
+
 
 class UserViewSet(ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]  # Agora requer autenticação
 
     @action(detail=False, methods=['post'], url_path='register', permission_classes=[AllowAny])
     def register(self, request):
@@ -34,3 +38,32 @@ class UserViewSet(ModelViewSet):
                 'access': str(refresh.access_token),
             })
         return Response({'detail': 'Credenciais inválidas'}, status=status.HTTP_401_UNAUTHORIZED)
+
+    @action(detail=True, methods=['post'], url_path='follow')
+    def follow(self, request, pk=None):
+        following_user = self.get_object()
+        follower_user = request.user
+
+        if follower_user == following_user:
+            return Response({"detail": "Você não pode seguir a si mesmo."}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Verifica se o usuário já está seguindo
+        follow, created = Follow.objects.get_or_create(follower=follower_user, following=following_user)
+        if created:
+            return Response({"detail": f"Agora você está seguindo {following_user.username}."}, status=status.HTTP_201_CREATED)
+        return Response({"detail": "Você já está seguindo esse usuário."}, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=['post'], url_path='unfollow')
+    def unfollow(self, request, pk=None):
+        following_user = self.get_object()
+        follower_user = request.user
+
+        if follower_user == following_user:
+            return Response({"detail": "Você não pode deixar de seguir a si mesmo."}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Verifica se o usuário segue o outro
+        follow = Follow.objects.filter(follower=follower_user, following=following_user).first()
+        if follow:
+            follow.delete()
+            return Response({"detail": f"Você deixou de seguir {following_user.username}."}, status=status.HTTP_204_NO_CONTENT)
+        return Response({"detail": "Você não está seguindo esse usuário."}, status=status.HTTP_400_BAD_REQUEST)
